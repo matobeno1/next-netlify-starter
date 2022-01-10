64be5f96bb01471c2b8d59a743699cd5cd473cb5
Matej Beno <matobeno1@gmail.com>
2022-01-10 16:26:52 +0100

    Added form to netlify

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,19 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e);
+
+    let formData = new FormData(e.target)
+    fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    }).then(() => console.log('Form successfully submitted')).catch((error) =>
+        alert(error))
+}
+
 export default function Home() {
   return (
     <div className="container">
@@ -11,10 +24,15 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+              <input type="hidden" name="form-name" value="contact"/>
+              <p>
+                  <label>Your Name: <input type="text" name="name" /></label>
+              </p>
+              <p>
+                  <button type="submit">Send</button>
+              </p>
+          </form>
       </main>
 
       <Footer />
